4779a91ffd93b9d2ef42c251f3fe1a50432975a0
Frederik Vig <frederik@geta.no>
2017-10-19 12:39:42 +0200

    Deleted old Epi 7 stuff

--- CMS7/Dds/Responses/StringResponse.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Geta.DdsAdmin.Dds.Responses
-{
-    public class StringResponse : BaseResponse
-    {
-        public string Response { get; set; }
-        public bool NotJson { get; set; }
-    }
-}
